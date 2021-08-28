24470d2461b0e55a7ff0d53c8e00b4298bf6233d
Fhenzy <Diegosilva220799@gmail.com>
2021-08-28 12:26:36 -0300

    pagina pronta

---
+++ src/components/Clima/Clima.jsx
@@ -0,0 +1,64 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import { Painel, Centro, Title, Address, Photo} from './styles'
+import Atual from '../../assets/medidor.jpg';
+
+const Clima = () => {
+
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState (false)
+ 
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+         lat: lat,
+         lon: long,
+         appid: process.env.REACT_APP_API_DE_CLIMA,
+         lang: 'pt',
+         units: 'metric'
+      }
+    });
+    setWeather(res.data);
+    console.log(res.data)
+  }
+
+   useEffect(() => {
+     navigator.geolocation.getCurrentPosition((position) => {
+    getWeather(position.coords.latitude, position.coords.longitude);
+    setLocation (true)
+  })
+   }, [])
+
+   if (location === false) {
+     return (
+       <div className="App">
+       Você precisa habilitar a localização no browser!
+       </div>
+     )
+   } else if (weather === false){
+     return (
+       <>
+       Carregando o clima ...
+       </>
+     )
+   }
+
+  else {
+    return (
+    <Painel className="App">
+      <Centro>
+      <Title>Temperatura Neste Momento :</Title>
+        <Address>
+        {weather['main']['temp']}º<br/>
+        {weather['weather'][0]['description']}
+        </Address>
+        
+      </Centro>
+        <Photo src= {Atual} alt='logo do medidor' ></Photo>
+    </Painel>
+  );
+}
+}
+
+export default Clima; 
+
